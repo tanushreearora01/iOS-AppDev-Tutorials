fbcb75b55bc70423c03bd26d61bc0460b8161750
Tanushree Arora <aroratanushree.28@gmail.com>
2024-04-10 23:48:58 -0400

    creating a card view

---
+++ Scrumdinger/TrailingIconLabelStyle.swift
@@ -0,0 +1,24 @@
+//
+//  TrailingIconLabelStyle.swift
+//  Scrumdinger
+//
+//  Created by Tanushree Arora on 4/10/24.
+//
+
+import SwiftUI
+
+struct TrailingIconLabelStyle: LabelStyle {
+    func makeBody(configuration: Configuration) -> some View {
+        HStack {
+            configuration.title
+            configuration.icon
+        }
+        
+    }
+}
+
+extension LabelStyle where Self == TrailingIconLabelStyle {
+    static var trailingIcon: Self { Self() }
+}
+
+
