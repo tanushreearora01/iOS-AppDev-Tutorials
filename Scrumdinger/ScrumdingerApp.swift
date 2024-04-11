866d9c88056e4fe26ffbf766d3c86be54b32e2de
Tanushree Arora <aroratanushree.28@gmail.com>
2024-04-10 23:07:21 -0400

    Stack Learning

---
+++ Scrumdinger/ScrumdingerApp.swift
@@ -0,0 +1,17 @@
+//
+//  ScrumdingerApp.swift
+//  Scrumdinger
+//
+//  Created by Tanushree Arora on 4/10/24.
+//
+
+import SwiftUI
+
+@main
+struct ScrumdingerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            MeetingView()
+        }
+    }
+}
